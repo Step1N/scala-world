d3f4fe4d3fd59a4d53df4e219f50601dc3aa0b9d
durgeshg <durgesh.gaur@brambles.com>
2018-12-12 18:12:52 +0530

    Renamed controller

--- app/controllers/WorldController.scala
+++ app/controllers/WorldController.scala
@@ -1,6 +1,8 @@
+package controllers
+
 import play.api.mvc.{Action, Controller}
 
-object AppController extends Controller {
+object WorldController extends Controller{
 
   def ping = Action { implicit request =>
     Ok("""{"msg": "pong from scala world"}""").as("application/json")
